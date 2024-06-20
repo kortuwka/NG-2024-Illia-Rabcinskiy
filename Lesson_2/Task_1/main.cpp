79d97a077f469c05c276bdbdaf5188a92c4e734f
Illia Rabchinkiy <you@example.com>
2024-06-20 17:43:37 +0300

    corrected

--- Lesson_2/Task_1/main.cpp
+++ Lesson_2/Task_1/main.cpp
@@ -3,59 +3,56 @@
 using namespace std;
 
 int main(){
-    bool while_1 = true;
+    bool isRunning = true;
     int schet;
     float schet_minus;
     float schet_plus;
     int schet_cin;
-    int mas[10] = {0,0,0,0,0,0,0,0,0,0};
-    while (while_1 == true){
+    int bankAccounts[10] = {0,0,0,0,0,0,0,0,0,0};
+    while (isRunning == true){
         cout << endl;
         cout << "If you want to deposit money, write 1, if you want to withdraw, write 2, if you want to view all accounts, then 3, if you close the program, then 4";
         cout << endl;
         cin >> schet;
-        switch (schet){
-        case 1:
-            cout << endl;
-            cout << "How many?";
-            cin >> schet_plus;
+        if (schet == 1 || schet == 2){
             cout << endl;
             cout << "On which one?";
             cin >> schet_cin;
-            mas[schet_cin] = mas[schet_cin] + schet_plus;
+            cout << endl;
+            cout << "How many?";
+        }
+        switch (schet){
+        case 1:
+            cin >> schet_plus;
+            bankAccounts[schet_cin] = bankAccounts[schet_cin] + schet_plus;
             cout << endl;
             for (int row = 0; row < 10;row++){
                 cout << " ";
-                cout << mas[row];
+                cout << bankAccounts[row];
             }
             break;
         case 2:
-            cout << endl;
-            cout << "How many?";
             cin >> schet_minus;
-            cout << endl;
-            cout << "On which one?";
-            cin >> schet_cin;
-            if (mas[schet_cin] < 0){
+            if (bankAccounts[schet_cin] < 0){
                 cout << endl;
                 cout << "Credit on this account";
             }else{
-            mas[schet_cin] = mas[schet_cin] - schet_minus;
+            bankAccounts[schet_cin] = bankAccounts[schet_cin] - schet_minus;
             }
             cout << endl;
             for (int row = 0; row < 10;row++){
                 cout << " ";
-                cout << mas[row];
+                cout << bankAccounts[row];
             }
             break;
         case 3:
             for (int row = 0; row < 10;row++){
                 cout << " ";
-                cout << mas[row];
+                cout << bankAccounts[row];
             }
             break;
         case 4:
-            while_1 = false;
+            isRunning = false;
             break;
         }
     }
