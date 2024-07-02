744dfe7218ddf898c2a4665a0b3e0fb9bf56c142
Illia Rabchinkiy <you@example.com>
2024-07-02 23:08:37 +0300

    Task_1

---
+++ Lesson_5/Task_1/mainwindow.cpp
@@ -0,0 +1,29 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+#include "secondwindow.h"
+#include <QApplication>
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    SecondWindow window;
+    window.setModal(true);
+    window.exec();
+}
+
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    QApplication::quit();
+}
+
