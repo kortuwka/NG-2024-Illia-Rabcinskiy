9ac3513977abcfde61e2b703711a163b1d6eea0e
Illia Rabchinkiy <you@example.com>
2024-07-04 21:33:24 +0300

    corrected

--- Lesson_2/Task_4/main.cpp
+++ Lesson_2/Task_4/main.cpp
@@ -1,30 +1,22 @@
 #include <iostream>
 
-
 using namespace std;
 
 int main(){
-    char Line;
-    int chisla = 0;
+    char Line[80];
+
     int space = 0;
-    cin.get(Line);
-    while(Line != '\n')
+    cin.getline(Line,80);
+
+   for(int i=0; Line[i] != '\0' ; i++)
     {
-        if(Line==' ') {
+        if((isalpha(Line[i])) !=(isalpha(Line[i+1]))) {
+                if(isalpha(Line[i]))
                     space++;
         }
-        if (Line == '1' || Line == '0' || Line == '2' || Line == '3' || Line == '4' || Line == '5' || Line == '6' || Line == '7' || Line == '8' || Line == '9'){
-            if(chisla == 0){
-                space++;
-                chisla++;
-            }
-        }else{
-            chisla = 0;
-        }
-        cin.get(Line);
+
     }
     cout << endl;
-    space++;
     cout << "word count = ";
     cout << space;
     return 0;
