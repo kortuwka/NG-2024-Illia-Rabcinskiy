f28b39010b4a300a2eeefbab48b91d9e2997e66e
Illia Rabchinkiy <you@example.com>
2024-07-04 12:08:18 +0300

    corrected

--- Lesson_5/Task_4/build/Desktop_Qt_6_7_1_llvm_mingw_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_Task_4_FILE D:/Lesson_5/Task_4/build/Desktop_Qt_6_7_1_llvm_mingw_64_bit-Debug/Task_4.exe)
-set(__QT_DEPLOY_TARGET_Task_4_RUNTIME_DLLS C:/Qt/6.7.1/llvm-mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.1/llvm-mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.1/llvm-mingw_64/bin/Qt6Core.dll)
