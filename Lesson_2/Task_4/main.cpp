7055f596db54e1aa4b08c91f25a1c3ac85a2870d
Illia Rabchinkiy <you@example.com>
2024-07-04 20:27:21 +0300

    corrected

--- Lesson_2/Task_4/main.cpp
+++ Lesson_2/Task_4/main.cpp
@@ -1,14 +1,28 @@
 #include <iostream>
+#include <vector>
+#include <cctype>
 
 using namespace std;
 
 int main(){
     char Line;
+    int chisla = 0;
+    //int atoi(Line);
     int space = 0;
     cin.get(Line);
     while(Line != '\n')
     {
-        if(Line == ' ') {space++;}
+        if(Line==' ') {
+                    space++;
+        }
+        if (Line == '1' || Line == '0' || Line == '2' || Line == '3' || Line == '4' || Line == '5' || Line == '6' || Line == '7' || Line == '8' || Line == '9'){
+            if(chisla == 0){
+                space++;
+                chisla++;
+            }
+        }else{
+            chisla = 0;
+        }
         cin.get(Line);
     }
     cout << endl;
