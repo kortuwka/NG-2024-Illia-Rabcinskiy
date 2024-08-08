780b5932043ddd9950a63501f5d529da103162a9
Illia Rabchinkiy <illarabcinskij9@gmail.com>
2024-08-08 19:14:56 +0300

    Task_1 corrected

--- Lesson_8/Task_1/aiaskeverything.cpp
+++ Lesson_8/Task_1/aiaskeverything.cpp
@@ -25,38 +25,35 @@ AIAskEverything::~AIAskEverything()
 QNetworkReply *reply;
 QByteArray Question;
 QString Check_for_characters;
+QString Balls_corrected;
+QString Numberoftasks_String;
 std::string Check_for_characters_corrected;
 int Balls = 0;
-int numberofanswers;
 int val;
+int Numberoftasks = 0;
 
 void AIAskEverything::askAi()
 {
     QString text_from_question = ui->te_question->toPlainText();
     std::string text_from_question_corrected = text_from_question.toLocal8Bit().constData();
-    val = text_from_question_corrected.find("%#");
-    qDebug() << val;
-    if(val == 1){
-        ui->te_answer->setText("Please type without symbols %#, )*");
-    }else{
-        QNetworkRequest request(QUrl("http://127.0.0.1:11434/api/generate"));
-        request.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
 
-        QString answerstr = ui->te_answer->toPlainText();
+    QNetworkRequest request(QUrl("http://127.0.0.1:11434/api/generate"));
+    request.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
 
-        Question = answerstr.toUtf8();
+    QString answerstr = ui->te_answer->toPlainText();
 
-        QByteArray examination = "write if the answer is correct, if yes then answer true without unnecessary text, if no then answer false without unnecessary text, here is the question: " + Question + " here is the answer: ";
-        QString text = examination + ui->te_question->toPlainText();
-        ui->te_answer->setText("");
+    Question = answerstr.toUtf8();
 
-        QJsonObject json;
-        json["model"] = "llama3.1:8b";
-        json["prompt"] = text;
-        json["stream"] = false;
+    QByteArray examination = "write if the answer is correct, if yes then answer true without unnecessary text, if no then answer false without unnecessary text, here is the question: " + Question + " here is the answer: ";
+    QString text = examination + ui->te_question->toPlainText();
+    ui->te_answer->setText("");
 
-        reply = m_manager->post(request, QJsonDocument(json).toJson());
-    }
+    QJsonObject json;
+    json["model"] = "llama3.1:8b";
+    json["prompt"] = text;
+    json["stream"] = false;
+
+    reply = m_manager->post(request, QJsonDocument(json).toJson());
 }
 
 void AIAskEverything::newAnswer (QNetworkReply *reply)
@@ -126,26 +123,25 @@ void AIAskEverything::on_Check_clicked()
     if(val == -1){
         val = Check_for_characters_corrected.find("True");
     }
-
-    if(val == 0){
-        Balls = Balls + 1;
-        numberofanswers = numberofanswers + 1;
-        qDebug() << Balls;
-        if(Balls != 0){
-            QString Balls_corrected = QString::number(Balls) + "/" + QString::number(numberofanswers);
-            ui->Balls->setText(Balls_corrected);
-        }
-    }else if(val == -1){
-        if(Balls != 0){
-            numberofanswers = numberofanswers + 1;
+    if(Numberoftasks == 10){
+        Numberoftasks = 0;
+        ui->Balls->setText(Balls_corrected);
+    }else{
+        Numberoftasks = Numberoftasks + 1;
+        if(val == 0){
+            Balls = Balls + 1;
             qDebug() << Balls;
-            QString Balls_corrected = QString::number(Balls) + "/" + QString::number(numberofanswers);
-            ui->Balls->setText(Balls_corrected);
-        }else{
-            numberofanswers = numberofanswers + 1;
-            QString Balls_corrected = QString::number(Balls) + "/" + QString::number(numberofanswers);
-            ui->Balls->setText(Balls_corrected);
+            Balls_corrected = QString::number(Balls) + "/10";
+        }else {
+            if(Balls != 0){
+                qDebug() << Balls;
+                Balls_corrected = "Balls: " + QString::number(Balls) + "/10";
+            }else{
+                Balls_corrected = "Balls: " + QString::number(Balls) + "/10";
+            }
         }
+        Numberoftasks_String = "Task: " + QString::number(Numberoftasks) + "/10";
+        ui->Balls->setText(Numberoftasks_String);
     }
 }
 
